291a3b8bf73b30a4c625252e7fd0fa3f20fa62d9
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-20 17:26:46 +0800

    Added videofeed component skeleton

---
+++ frontend/src/component/webcam/video.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VideoFeed extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <video src=""></video>
+        );
+    }
+}
+ 
+export default VideoFeed;
+
