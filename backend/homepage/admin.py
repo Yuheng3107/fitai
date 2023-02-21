ae488fc1872eed7b930bb8c60f952f7c4082e9fc
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-21 16:07:52 +0800

    Added Custom User Model on Website

--- backend/homepage/admin.py
+++ backend/homepage/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import AppUser
 
-# Register your models here.
+from .forms import AppUserCreationForm, AppUserChangeForm
+from .models import AppUser
+
+class AppUserAdmin(UserAdmin):
+    add_form = AppUserCreationForm
+    form = AppUserChangeForm
+    model = AppUser
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 admin.site.register(AppUser, UserAdmin)
--- backend/homepage/models.py
+++ backend/homepage/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import AppUserManager
+
 # Create your models here.
 
 class AppUser(AbstractUser):
-    pass
+    
+    # Sets username to anything the user sets to be, blank=True makes username optional in forms
+    username = models.CharField(max_length=50, blank=True)
+    # Removes need to put password as auth is done using Social Login
+    email = models.EmailField(_('email address'), max_length=100, unique=True)
+    
+    # Makes email the unique identifier of a entry, it is now the "username" which means unique identifier
+    USERNAME_FIELD = 'email'
+    # Makes email required
+    REQUIRED_FIELDS = []
+
+    objects = AppUserManager()
+
+    def __str__(self):
+        return self.email
