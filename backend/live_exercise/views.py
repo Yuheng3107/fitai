f5140489f734c801646db21a6aa713876b0f079a
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-21 23:31:59 +0800

    Added skeleton of APIView in live_exercises

--- backend/live_exercise/views.py
+++ backend/live_exercise/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HandleKeyPoints(APIView):
+    def post(self, request):
+        # Data is all in request
+        # Put data in json format in Response
+        return Response()
+        pass
