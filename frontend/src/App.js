d0984dec869b1058e3c6a10c348f5ef552a4cb2d
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-02-21 10:13:14 +0800

    shifted over index.html from original project, nothing is working yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,12 +31,12 @@ function App() {
             access_token: user.access_token,
           }
         )
-        .then((res) => {
-          console.log(res)
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+          .then((res) => {
+            console.log(res)
+          })
+          .catch((err) => {
+            console.log(err);
+          });
         // axios
         //   .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
         //     headers: {
@@ -62,8 +62,42 @@ function App() {
   return (
     <div>
       <Navbar />
-      <span className="text-2xl">testing tailwindCSS text</span>
-      <main></main>
+      <span classNameName="text-2xl">testing tailwindCSS text</span>
+      <main>
+        <canvas id="canvas" className="hidden"></canvas>
+        <video id="video" className="video-feed" autoplay playsinline alt="Video"></video>
+        <div id="rep-info-group">
+          <span id="rep-count"></span>
+          <p id="rep-feedback"></p>
+        </div>
+        <p id="main-feedback-group" className="text-center">
+          <span id="main-feedback" className="text-center"></span>
+        </p>
+        <a id="feedback-button" className="btn btn-secondary" href="/errors" target="_blank">About Errors</a>
+        <p id="emotion-feedback"></p>
+        <button id="show-log-button">
+          <span>Show Feedback Log</span>
+          <img src="../assets/svg/expand-icon.svg" alt="expand icon" />
+        </button>
+        <ul id="feedback-list"></ul>
+        <div id="start-button-group" className="flex">
+          <button id="start-button" className="btn btn-success mt-3">Start Camera</button>
+          <a className="btn btn-secondary mt-3" href="instructions">Instructions</a>
+        </div>
+
+        <form class="flex-row mt-3" id="changeExercise">
+          <select class="form-select" name="exerciseId" id="changeExercise">
+            <option selected value="0">Squat (Right Side)</option>
+            <option value="1">Squat (Front)</option>
+            <option value="2">Push-Up (Right Side)</option>
+          </select>
+          <input class="ms-2 btn btn-outline-info d-inline" type="submit" value="Start Exercise" />
+        </form>
+        <button id="end-button" class="btn btn-secondary">End Exercise</button>
+        <div id="alerter"></div>
+      </main>
+
+
       <div>
         {Object.keys(profile).length ? (
           <div>
@@ -88,7 +122,7 @@ function App() {
             console.log('Login Failed');
           }}
         />;
-        <p>Login State:<span className=''></span></p>
+        <p>Login State:<span classNameName=''></span></p>
       </div> */}
     </div>
   );
