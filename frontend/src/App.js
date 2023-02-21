5da876f9c4ae33bc09e3f1092d95dcf2178c1cbd
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-02-21 10:30:49 +0800

    refactored Login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,12 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { googleLogout, useGoogleLogin } from '@react-oauth/google'
-import { GoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
 
 import Navbar from './component/navbar/Navbar';
 import Main from './component/main/Main';
+import Login from './component/login/Login';
 
 
 function App() {
-  const [user, setUser] = useState({});
-  const [profile, setProfile] = useState({});
-
-  const login = useGoogleLogin({
-    onSuccess: (codeResponse) => {
-      console.log(codeResponse);
-      setUser(codeResponse);
-    },
-    onError: (error) => console.log('Login Failed:', error)
-  });
-
-  useEffect(
-    () => {
-      console.log("useEffect is running");
-      if (Object.keys(user).length) {
-        console.log("user object not empty");
-
-        axios.post(
-          "http://localhost:8000/rest-auth/google/",
-          {
-            access_token: user.access_token,
-          }
-        )
-          .then((res) => {
-            console.log(res)
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-        // axios
-        //   .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-        //     headers: {
-        //       Authorization: `Bearer ${user.access_token}`,
-        //       Accept: 'application/json'
-        //     }
-        //   })
-        //   .then((res) => {
-        //     console.log(res);
-        //     setProfile(res.data);
-        //   })
-        //   .catch((err) => console.log(err));
-      }
-    },
-    [user]
-  );
-
-  const logOut = () => {
-    googleLogout();
-    setProfile(null);
-  }
+  
 
   return (
     <div>
@@ -66,22 +14,8 @@ function App() {
       <span classNameName="text-2xl">testing tailwindCSS text</span>
       <Main />
 
-
-      <div>
-        {Object.keys(profile).length ? (
-          <div>
-            <img src={profile.picture} alt="user image" />
-            <h3>User Logged in</h3>
-            <p>Name: {profile.name}</p>
-            <p>Email Address: {profile.email}</p>
-            <br />
-            <br />
-            <button onClick={logOut}>Log out</button>
-          </div>
-        ) : (
-          <button onClick={() => login()}>Sign in with Google 🚀 </button>
-        )}
-      </div>
+      <Login />
+      
       {/* <div>
         <GoogleLogin
           onSuccess={credentialResponse => {
