11d8f7d9c63282e10977488906fbdc8777b92b80
Eyewhy <ivanyapweiwang@gmail.com>
2023-03-02 20:48:09 +0800

    add new exercises (unbalanced

--- frontend/components/webcam/video.jsx
+++ frontend/components/webcam/video.jsx
@@ -16,11 +16,6 @@ import "@tensorflow/tfjs-backend-webgl";
 //formCorrection
 import * as formCorrection from "../../utils/formCorrection.js";
 
-async function delay(ms) {
-  // return await for better async stack trace support in case of errors.
-  return await new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 class VideoFeed extends Component {
   constructor(props) {
     super(props);
@@ -88,8 +83,13 @@ class VideoFeed extends Component {
     );
   };
 
-  ////////
-  // TF movenet
+  /*--------------------
+  MOVENET METHODS
+  --------------------*/
+
+  /**
+   * Starts Exercise
+   */
   start = async () => {
     console.log("start");
 
@@ -106,48 +106,17 @@ class VideoFeed extends Component {
     this.generalFeedback.current.changeText("Loading...");
 
     // get from backend
-    let evalposes = [new Float32Array([0, 0, 0, 0, 1.05, 0, 0, 0, 0.7, 0, 0])];
-    let angleweights = new Float32Array([0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0]);
-    let anglethresholds = [
-      [
-        new Float32Array(2),
-        new Float32Array(2),
-        new Float32Array(2),
-        new Float32Array(2),
-        new Float32Array([0.14, 0.13]),
-        new Float32Array(2),
-        new Float32Array(2),
-        new Float32Array(2),
-        new Float32Array([0.15, 0]),
-        new Float32Array(2),
-        new Float32Array(2),
-      ],
-    ];
-    let glossary = [
-      [
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["Squat not low enough", "Squat too low"],
-        ["", ""],
-        ["", ""],
-        ["", ""],
-        ["Leaning forward too much", ""],
-        ["", ""],
-        ["", ""],
-      ],
-    ];
+    let exercise = getExercise(0);
 
     // initialise form correction
     formCorrection.init(
-      evalposes,
-      0.7,
-      0.02,
-      angleweights,
-      anglethresholds,
-      2000,
-      glossary
+      exercise.evalPoses,
+      exercise.scoreThreshold,
+      exercise.scoreDeviation,
+      exercise.angleWeights,
+      exercise.angleThresholds,
+      exercise.minRepTime,
+      exercise.glossary
     );
 
     while (this.isActive) {
@@ -177,6 +146,10 @@ class VideoFeed extends Component {
       */
     }
   };
+
+  /**
+   * Ends Exercise
+   */
   end = () => {
     this.isActive = false;
     console.log("End");
@@ -184,16 +157,16 @@ class VideoFeed extends Component {
     this.generalFeedback.current.changeText(this.frameCount);
   };
 
-  /*--------------------
-  HELPER FUNCTIONS
-  --------------------*/
+/*--------------------
+HELPER FUNCTIONS
+--------------------*/
   assignImgHeight = () => {
     let screenshot = this.webcam.current.getScreenshot();
     this.image.current.src = screenshot;
     let img = new Image();
     img.src = screenshot;
     img.onload = () => {
-      window.alert(`Width is ${img.width}, Height is ${img.height}`);
+      // window.alert(`Width is ${img.width}, Height is ${img.height}`);
       // Changes height and width of video in Webcam component
 
       // set explicit width and height for video
@@ -204,5 +177,91 @@ class VideoFeed extends Component {
     };
   }
 }
+
+async function delay(ms) {
+  // return await for better async stack trace support in case of errors.
+  return await new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+/**
+ * To be replaced with request to backend. 
+ * @param {Object} x 
+ * @returns Exercise Parameters
+ */
+function getExercise(x) {
+  if (x == 0) return {
+    evalPoses: [new Float32Array([0, 0, 0, 0, 1.05, 0, 0, 0, 0.7, 0, 0])],
+    scoreThreshold: 0.7,
+    scoreDeviation: 0.02,
+    angleWeights: new Float32Array([0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0]),
+    angleThresholds: [
+      [
+        new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),
+        new Float32Array([0.14, 0.13]),
+        new Float32Array(2),new Float32Array(2),new Float32Array(2),
+        new Float32Array([0.15, 0]),
+        new Float32Array(2),new Float32Array(2)
+      ]
+    ],
+    minRepTime: 2000,
+    glossary: [
+      [
+        ["", ""],["", ""],["", ""],["", ""],
+        ["Squat not low enough", "Squat too low"],
+        ["", ""],["", ""],["", ""],
+        ["Leaning forward too much", ""],
+        ["", ""],["", ""],
+      ],
+    ]
+  }
+  if (x == 1) return {
+    evalPoses: [new Float32Array([0.,0.,0.,0.,0.,2.375,0.,2.25,0.,0.,0.],)],
+    scoreThreshold: 0.7,
+    scoreDeviation: 0.02,
+    angleWeights: new Float32Array([0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0]),
+    angleThresholds: [
+      [
+        new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),
+        new Float32Array([0.26, 0.]),
+        new Float32Array(2),
+        new Float32Array([0.3, 0.2]),
+        new Float32Array(2),new Float32Array(2),new Float32Array(2)
+      ]
+    ],
+    minRepTime: 2000,
+    glossary: [
+      [
+        ['',''],['',''],['',''],['',''],['',''],
+        ['Knees collapse inwards',''],
+        ['',''],
+        ['Squat not low enough','Squat too low'],
+        ['',''],['',''],['',''],
+      ],
+    ]
+  }
+  if (x == 2) return {
+    evalPoses: [new Float32Array([0.,0.,0.,0.,2.825,0.,2.832,0.,1.583,0.,1.7],)],
+    scoreThreshold: 0.7,
+    scoreDeviation: 0.02,
+    angleWeights: new Float32Array([0.,0.,0.,0.,0.,0.,0.,0.,-1.,0.,10.]),
+    angleThresholds: [
+      [
+        new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),new Float32Array(2),
+        new Float32Array([0., 0.23]),
+        new Float32Array(2),
+        new Float32Array([0.3, 0])
+      ]
+    ],
+    minRepTime: 1500,
+    glossary: [
+      [
+        ['',''],['',''],['',''],['',''],['',''],['',''],['',''],['',''],
+        ['','Sagging back'],
+        ['',''],
+        ['Not going low enough','']
+      ],
+    ]
+  }
+} 
 
 export default VideoFeed;
