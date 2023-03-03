b802eba01932386495dee4bd4881d1cd471abaf4
Kuang Yu Heng <yuheng3107@gmail.com>
2023-03-03 12:07:25 +0800

    Added daphne to installed apps in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
