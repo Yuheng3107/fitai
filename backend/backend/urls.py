84403a1d7d5b6f1472cf43c0c79c6327ddfe1997
Kuang Yu Heng <yuheng3107@gmail.com>
2023-03-03 12:12:01 +0800

    Removed catchall view and added chat urls to urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
-from .views import catchall
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('live_exercise/', include('live_exercise.urls')),
-    re_path(r'', catchall),
+    path("chat/", include("chat.urls")),
     
 ]
