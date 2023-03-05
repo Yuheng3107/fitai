d50feaffee44e10da29f2e05a07eb059267982b3
Kuang Yu Heng <yuheng3107@gmail.com>
2023-03-05 12:29:28 +0800

    Added ChatGroup and ChatMessage models

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+class ChatGroup(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(get_user_model())
+
+
+class ChatMessage(models.Model):
+    sent_at = models.DateTimeField(auto_now=True)
+    # Delete all messages user sent if user is deleted
+    sender = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_DEFAULT, default="Deleted User")
+    content = models.CharField(max_length=1000)
+    # Delete all messages if the group is deleted
+    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
