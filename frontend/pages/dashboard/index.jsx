b71a46e3d63a3349863b777c12fb3fb933b91ad9
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-03-07 17:42:05 +0800

    moved googleOauthProvider to _app so it can be used across all components

---
+++ frontend/pages/dashboard/index.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../../components/navbar/Navbar";
+
+function Dashboard() {
+    return <>
+        <Navbar />
+        <h1>This is the dashboard page</h1>
+    </>
+}
+
+export default Dashboard;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,14 +3,12 @@ import Image from "next/image";
 import React from "react";
 import App from "./App";
 import reportWebVitals from "../utils/reportWebVitals";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
 function Home() {
-    return <GoogleOAuthProvider clientId="908101547092-2cg5rblc0ppg7dvn8csk6l8p8ehc6crt.apps.googleusercontent.com">
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </GoogleOAuthProvider>
+    return
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 }
 
 // If you want to start measuring performance in your app, pass a function
