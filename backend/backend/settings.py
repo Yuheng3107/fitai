2c0d624b9d8176bc60fd1cfec6a9da2dabe07ee0
Kuang Yu Heng <yuheng3107@gmail.com>
2023-03-08 18:39:56 +0800

    Edited settings to use json for tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,7 +85,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
