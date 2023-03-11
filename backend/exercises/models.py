6d62216c0370c66b960428f988414ba4e04afbe0
Eyewhy <ivanyapweiwang@gmail.com>
2023-03-11 13:27:26 +0800

    remove duplicate description field from exercise regime

--- backend/exercises/models.py
+++ backend/exercises/models.py
@@ -27,7 +27,6 @@ class ExerciseStatistics(models.Model):
 
 class ExerciseRegime(FeedPost):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=10000)
     exercises = models.ManyToManyField('Exercise',through='ExerciseDetails')
     times_completed = models.PositiveIntegerField(default=0)
 
