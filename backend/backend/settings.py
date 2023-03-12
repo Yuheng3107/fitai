e1c8294ac0224f14f03e421ac157cc3d0f800383
Kuang Yu Heng <yuheng3107@gmail.com>
2023-03-12 18:18:56 +0800

    Migrated db to postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,9 +102,13 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flex', 
+        'USER': 'admin', 
+        'PASSWORD': 'P@ssword1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
