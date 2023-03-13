a626e4f046ac5449289f9a4ef90b4b79d5396767
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-03-13 09:30:37 +0800

    added some other ionic imports

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,6 +5,20 @@ import "@ionic/react/css/core.css";
 import { setupIonicReact } from '@ionic/react';
 setupIonicReact();
 
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+
 //Google auth
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
