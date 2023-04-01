cf98a9a89b66b099c34c7186974405ad4c1f6c6c
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-04-01 21:31:32 +0800

    adding some elements to the rep selection page

--- flexfrontend/src/components/Exercise/ExerciseOptions.tsx
+++ flexfrontend/src/components/Exercise/ExerciseOptions.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
+
 import { IonButton } from "@ionic/react";
 import Button from "../ui/Button";
 
+
 type Props = {
-    startExerciseHandler:(event: React.MouseEvent<HTMLButtonElement>) => void;
+    startExerciseHandler: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-function ExerciseOptions ({startExerciseHandler}:Props) {
-    return <div>
+function ExerciseOptions({ startExerciseHandler }: Props) {
+    const [repCountInput, setRepCountInput] = useState(1);
+
+    const repIncrementHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    const repDecrementHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    return <div className="h-full flex flex-col justify-between">
         <span>These are the exercise options</span>
-        <Button onClick={startExerciseHandler} >Start Exercising</Button>
+        <div id="rep-select-container" className="flex justify-center">
+            <div id="rep-ring" className="bg-zinc-300">
+                <span>{repCountInput}</span>
+            </div>
+            <div className="flex flex-col">
+                <button>up</button>
+                <button>down</button>
+            </div>
+        </div>
+        <div className="mb-12 flex justify-center text-xl">
+            <Button className="px-6 py-4 bg-blue-400 hover:bg-blue-600 text-white" onClick={startExerciseHandler} >Start</Button>
+        </div>
     </div>
 }
 
