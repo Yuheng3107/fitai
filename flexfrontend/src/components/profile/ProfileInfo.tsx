4b268e877ffb8064a5586ea8329575a39d438c24
Eyewhy <ivanyapweiwang@gmail.com>
2023-04-11 23:38:10 +0800

    user profile more complete

--- flexfrontend/src/components/profile/ProfileInfo.tsx
+++ flexfrontend/src/components/profile/ProfileInfo.tsx
@@ -39,33 +39,56 @@ const ProfileInfo = ({ profileData }: ProfileInfoProps) => {
                 <span className="text-l">Followers</span>
             </div>
         </div>
-        <div id="toggle-bar" className="p-5 border border-zinc-500 p-2 rounded-lg w-10/12">
+        <div id="toggle-bar" className="p-5 border border-zinc-500 w-full">
 
         </div>
-        <div id="exercise-stats" className="flex flex-col justify-start w-full mt-2 h-screen">
-            <div className="flex flex-row justify-evenly w-full px-10">
-                <div className="flex flex-col border border-zinc-500 p-2 rounded-lg h-full">
+        <div id="exercise-stats" className="flex flex-col justify-start w-full mt-2 h-screen px-10">
+            <div className="flex flex-row justify-between w-full">
+                <div id="fav-exercise" className="flex flex-col border border-zinc-500 p-2 rounded-lg h-full w-5/12">
                     <span className="text-xs">Favourite Exercise</span>
-                    <span className="text-3xl">Squats</span>
+                    <span className="text-2xl">Squats</span>
                     <div>
-                        <span className="text-xs font-semibold">9320</span>
+                        <span className="text-sm font-semibold">9320</span>
                         <span className="text-xs"> Reps</span>
                     </div>
                     <div>
-                        <span className="text-xs font-semibold">89%</span>
-                        <span className="text-xs"> Perfect</span>
+                        <span className="text-sm font-semibold">89%</span>
+                        <span className="text-xs"> Perfect</span>   
                     </div>
+                    
                 </div>
-                <div className="flex flex-col justify-evenly">
-                    <div className="flex flex-col border border-zinc-500 p-2 rounded-lg">
+                <div className="flex flex-col justify-evenly w-5/12">
+                    <div id="streak" className="flex flex-col border border-zinc-500 p-2 rounded-lg">
                         <span className="text-xs">Longest Streak</span>
                         <span className="text-xl">324 days</span>
                     </div>
-                    <div className="flex flex-col border border-zinc-500 p-2 rounded-lg mt-2">
+                    <div id="calories" className="flex flex-col border border-zinc-500 p-2 rounded-lg mt-2">
                         <span className="text-xs">Calories Burnt</span>
                         <span className="text-xl">46000 kcal</span>
                     </div>
                 </div>
+            </div>
+            <div className="flex flex-row justify-evenly mt-2">
+                <div id="workout" className="flex flex-col border border-zinc-500 p-2 rounded-lg w-full">
+                    <span className="text-xs">Favourite Workout</span>
+                    <span className="text-3xl">HIIT</span>
+                    <div>
+                        <span className="text-sm font-semibold">42</span>
+                        <span className="text-xs"> Times</span>
+                    </div>
+                    <div>
+                        <span className="text-sm font-semibold">11</span>
+                        <span className="text-xs"> hrs </span>
+                        <span className="text-sm font-semibold">42</span>
+                        <span className="text-xs"> mins</span>
+                    </div>
+                </div>    
+            </div>
+            <div className="flex flex-row justify-evenly mt-2">
+                <div id="breakdown" className="flex flex-col border border-zinc-500 p-2 rounded-lg w-full">
+                    <span className="text-xs">Exercise Breakdown</span>
+                    <span className="text-3xl">IMAGE HERE</span>
+                </div>    
             </div>
         </div>
     </div>
