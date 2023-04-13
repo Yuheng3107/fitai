b668e689da09d49e67c47c253c77069aa83ef1ee
Kuang Yu Heng <yuheng3107@gmail.com>
2023-04-13 09:41:52 +0800

    Added negative margins to make key stats closer

--- flexfrontend/src/components/profile/KeyStats.jsx
+++ flexfrontend/src/components/profile/KeyStats.jsx
@@ -5,15 +5,15 @@ export default function KeyStats() {
       className="flex flex-row items-center justify-evenly w-full"
     >
       <div id="reps" className="flex flex-col items-center justify-evenly">
-        <span className="text-xl font-semibold m-0">12.4k</span>
+        <span className="text-xl font-semibold -mb-1">12.4k</span>
         <span className="text-l m-0">Repetitions</span>
       </div>
       <div id="perfect" className="flex flex-col items-center justify-evenly">
-        <span className="text-xl font-semibold">94%</span>
+        <span className="text-xl font-semibold -mb-1">94%</span>
         <span className="text-l">Perfect</span>
       </div>
       <div id="followers" className="flex flex-col items-center justify-evenly">
-        <span className="text-xl font-semibold">69</span>
+        <span className="text-xl font-semibold -mb-1">69</span>
         <span className="text-l">Followers</span>
       </div>
     </div>
