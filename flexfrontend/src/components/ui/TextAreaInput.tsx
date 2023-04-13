347be0884e867d0f081070abe0f67a5ca92766a8
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-04-13 22:47:18 +0800

    fixed the bio default value bug by making the EditProfile page wait until the profileData is loaded before loading in the input components

--- flexfrontend/src/components/ui/TextAreaInput.tsx
+++ flexfrontend/src/components/ui/TextAreaInput.tsx
@@ -6,18 +6,20 @@ type TextAreaInputProps = {
     placeholder?: string;
     className?: string;
     children?: ReactNode;
+    defaultValue?: string;
 }
 
 type Ref = HTMLTextAreaElement;
-
+// forwardRef is used here so we can reference this component when it's used in other components
 const TextAreaInput = forwardRef<Ref, TextAreaInputProps>((props, ref) => {
-    const [textInput, setTextInput] = useState<string>('');
+
+    const [textInput, setTextInput] = useState(props.defaultValue);
+    console.log(props.defaultValue);
     return <div className={`relative flex flex-col justify-start ${props.className}`}>
         <label className="text-neutral-700 text-sm resize-none" htmlFor={props.inputName}>{props.label}</label>
         <textarea value={textInput} className="p-1 mt-1 border rounded-xl border-b-zinc-400 bg-transparent" name={props.inputName}
             onChange={(event) => {
                 if (event.target.value.length <= 150) {
-                    console.log(textInput);
                     setTextInput(event.target.value)
                 } else {
                     let exceededLength = (event.target.value.length - 150) * -1;
@@ -25,9 +27,9 @@ const TextAreaInput = forwardRef<Ref, TextAreaInputProps>((props, ref) => {
                 }
             }}
             id={props.inputName} ref={ref} rows={5}>
-
+                {textInput}
         </textarea>
-        <span className={`absolute bottom-1 right-2 text-xs text-neutral-400`}>{textInput.length}/150</span>
+        <span className={`absolute bottom-1 right-2 text-xs text-neutral-400`}>{textInput ? textInput.length : 0}/150</span>
     </div>
 });
 
