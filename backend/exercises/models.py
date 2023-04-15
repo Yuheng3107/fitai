9ec25a15c08e82358f58df334a957bc86326ce5d
Kuang Yu Heng <yuheng3107@gmail.com>
2023-04-15 15:32:12 +0800

    Added exercise sessions models

--- backend/exercises/models.py
+++ backend/exercises/models.py
@@ -59,4 +59,11 @@ class ExerciseRegimeStatistics(models.Model):
         return f"{self.user} stats for {self.exercise_regime}"
 
 
-
+class ExerciseSessions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    exercise_regime = models.ForeignKey(ExerciseRegime, on_delete=models.SET_NULL, blank=True, null=True)
+    sets = models.PositiveSmallIntegerField(default=1)
+    duration = models.PositiveSmallIntegerField()
+    reps = models.PositiveSmallIntegerField(default=0)
+    perfect_reps = models.PositiveSmallIntegerField(default=0)
