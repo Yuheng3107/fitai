a4febd5744b088f10f030302b9b23932a0c8a6c6
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-04-20 18:02:31 +0800

    small edits to CreateCommunity.tsx

--- flexfrontend/src/pages/community/CreateCommunity.tsx
+++ flexfrontend/src/pages/community/CreateCommunity.tsx
@@ -1,10 +1,22 @@
-
+import React from 'react';
 import { IonPage, IonContent } from '@ionic/react';
+import { backend } from '../../App';
 
 function CreateCommunity() {
+
+    function createCommunityHandler(event: React.FormEvent) {
+        event.preventDefault();
+        fetch(`${backend}/community/community/create`, {
+            
+        })
+    }
+
     return <IonPage>
         <IonContent>
-            <input type="text" />
+            <form>
+
+                <input type="text" />
+            </form>
         </IonContent>
     </IonPage>
 }
