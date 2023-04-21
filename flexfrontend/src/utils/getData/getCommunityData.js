6e5bb791d0bc7cd8040a079f9fbdc695ba29e767
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-04-21 21:44:36 +0800

    some style updates for CommunityDisplay.tsx

--- flexfrontend/src/utils/getData/getCommunityData.js
+++ flexfrontend/src/utils/getData/getCommunityData.js
@@ -16,20 +16,3 @@ export const getCommunityAsync = async function (pk) {
     console.log(error);
   }
 }
-
-export const getCommunitiesListAsync = async () => {
-  try {
-    let res = await fetch(`${backend}/community/community/list`, {
-      method: "GET",
-      credentials: "include", // include cookies in the request
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": document.cookie?.match(/csrftoken=([\w-]+)/)?.[1],
-      },
-    })
-    let data = await res.json();
-    return data
-  } catch (error) {
-    console.log(error);
-  }
-}
