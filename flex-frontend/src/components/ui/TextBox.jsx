38488f4e4ea5a6ea9285188aeb38092177b3d62d
Kuang Yu Heng <yuheng3107@gmail.com>
2023-04-24 14:50:01 +0800

    Moved flex-frontend to a repo of its own'

--- flex-frontend/src/components/ui/TextBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function TextBox(props) {
-    return <span
-        id={props.id} className={`${props.className} p-3 rounded-xl`}>
-            {props.children}
-        </span>
-}
-export default TextBox;
--- flex-frontend/src/utils/compareObjects.tsx
+++
@@ -1,2 +0,0 @@
-export const profileDatasAreEqual = (profile1: object, profile2: object) => {
-}
